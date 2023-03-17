79687291fd99ff2f294a0966f95fd81494c09205
Nasroallah Elidrissi <nelidris@e2r10p1.1337.ma>
2023-03-17 13:33:55 +0100

    inception

--- srcs/requirements/mariadb/tools/m_db.sh
+++ srcs/requirements/mariadb/tools/m_db.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-echo "CREATE DATABASE IF NOT EXISTS $DB_NAME ;" > db1.sql
-echo "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS' ;" >> db1.sql
-echo "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' ;" >> db1.sql
+service mysql start
+
+echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE ;" > db1.sql
+echo "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' ;" >> db1.sql
+echo "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' ;" >> db1.sql
 echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'root' ;" >> db1.sql
 echo "FLUSH PRIVILEGES;" >> db1.sql
 
-mariadb < db1.sql
+mysql < db1.sql
 
 kill $(cat /var/run/mysqld/mysqld.pid)
-mariadbd
+
+mysqld
