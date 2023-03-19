befdbc60ebfeb60f620550a4e47dfb2a606cebdf
Nasroallah Elidrissi <nelidris@e2r10p5.1337.ma>
2023-03-19 14:56:17 +0000

    inception

---
+++ srcs/requirements/bonus/static_website/tools/deps/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.use('/static_website', express.static("/var/www/html"));
+
+app.listen(3000, () => {
+	console.log("Server started on port 3000");
+});
+
+app
