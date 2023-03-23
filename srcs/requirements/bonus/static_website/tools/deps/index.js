08451dbfa4137034f47ce75d17dc9c9bd9f54b96
Nasroallah Elidrissi <nelidris@e2r5p12.1337.ma>
2023-03-23 15:34:54 +0000

    inception

--- srcs/requirements/bonus/static_website/tools/deps/index.js
+++ srcs/requirements/bonus/static_website/tools/deps/index.js
@@ -4,6 +4,6 @@ const app = express();
 
 app.use('/static_website', express.static("/var/www/html"));
 
-app.listen(3000, () => {
-	console.log("Server started on port 3000");
+app.listen(3001, () => {
+	console.log("Server started on port 3001");
 });
--- srcs/requirements/wordpress/tools/wp.sh
+++ srcs/requirements/wordpress/tools/wp.sh
@@ -3,17 +3,26 @@
 # Set up WordPress
 
 mkdir -p /run/php
+mkdir -p /var/www/html
 
 chown www-data:www-data /var/www/html
 
-wp core download --allow-root --path=/var/www/html
+su www-data -s /bin/bash -c "wp core download --path=/var/www/html"
 
 sed -i 's/\/run\/php\/php7\.3-fpm\.sock/wordpress:9000/g' /etc/php/7.3/fpm/pool.d/www.conf
 
-wp core config --allow-root --path=/var/www/html --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb
+su www-data -s /bin/bash -c "wp core config --path=/var/www/html --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb"
 
-wp core install --allow-root --path=/var/www/html --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_USER --admin_password=$WP_PASSWORD --admin_email=$WP_EMAIL --ftp_host=ftp --ftp_user=$FTP_USER --ftp_pass=$FTP_PASS
+su www-data -s /bin/bash -c "wp core install --path=/var/www/html --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_USER --admin_password=$WP_PASSWORD --admin_email=$WP_EMAIL"
 
-wp --allow-root plugin install redis-cache --activate --path=/var/www/html
+su www-data -s /bin/bash -c "wp plugin install redis-cache --activate --path=/var/www/html"
+
+su www-data -s /bin/bash -c "wp config set FS_METHOD direct --type=constant --path=/var/www/html"
+su www-data -s /bin/bash -c "wp config set WP_REDIS_HOST redis --type=constant --path=/var/www/html"
+su www-data -s /bin/bash -c "wp config set WP_REDIS_PORT 6379 --type=constant --path=/var/www/html"
+su www-data -s /bin/bash -c "wp config set WP_CACHE true --type=constant --path=/var/www/html"
+
+su www-data -s /bin/bash -c "wp plugin install redis-cache --activate --path=/var/www/html"
+su www-data -s /bin/bash -c "wp redis enable --path=/var/www/html"
 
 php-fpm7.3 -R -F
