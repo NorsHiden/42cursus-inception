c9fcc00e49ac659187ab9ed12d0d2f3807af0e8a
nelidris <nelidris@student.1337.ma>
2023-06-27 23:36:23 +0100

    inception

--- srcs/requirements/bonus/static_website/tools/deps/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const app = express();
-
-app.use('/static_website', express.static("/var/www/html"));
-
-app.listen(3001, () => {
-	console.log("Server started on port 3001");
-});
